be578ba9c92b88eb91e55a03a6842c856566a338
zhangkaiyi <zhangkaiyi@yidian-inc.com>
2018-04-03 21:57:08 +0800

    20

---
+++ 20-ValidParentheses.js
@@ -0,0 +1,24 @@
+var isValid = function(s) {
+  var charList = {
+    '(' : ')',
+    '{' : '}',
+    '[' : ']' 
+  }
+  var arr = [];
+  if(!s.length || s.length % 2) {
+    return false;
+  } else {
+    for(var i = 0; i < s.length; i++) {
+      if(charList[s[i]]) {
+        arr.push(charList[s[i]]);
+      } else if(!arr.length || arr.pop() != s[i]) {
+        return false;
+      }
+    }
+    return arr.length == 0;
+  }
+};
+console.log('()', isValid('()'));
+console.log('()[]{}', isValid('()[]{}'));
+console.log('([)]', isValid('([)]'));
+console.log('([])', isValid('([])'));
